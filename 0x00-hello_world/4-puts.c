941bd69c4e07c4820741846413e1ce297f219a94
Saviourise <saviourise1@gmail.com>
2022-12-08 01:33:05 -0800

    C 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - prints out "\"programming is like building a multilingual puzzle"
+ * Return: 0 if it run completely
+*/
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
