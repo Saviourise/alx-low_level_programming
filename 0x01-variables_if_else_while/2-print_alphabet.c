28e0e0e0143adb14ca76de8bd717791172048c57
Saviourise <saviourise1@gmail.com>
2022-12-09 07:48:53 -0800

    If 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+
+
+/**
+
+ * main- prints all alphabet in order.
+
+ * Return: 0 if successful.
+
+ */
+
+
+
+int main(void)
+
+{
+
+char az;
+
+
+
+for (az = a; az <= z; az++)
+
+{
+
+putchar(az);
+
+}
+
+putchar(n);
+
+return (0);
+
+}
