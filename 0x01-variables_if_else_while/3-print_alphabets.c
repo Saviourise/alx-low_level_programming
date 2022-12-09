df9068c014423d9edd590b2b3ad10d19e7d78d9d
Saviourise <saviourise1@gmail.com>
2022-12-09 07:58:56 -0800

    If 3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main- print lower case and upper case
+ * alphabet in one line and followed by
+ * new line
+ * Return: 0 if successful.
+ */
+int main(void)
+{
+char az;
+for (az = a; az <= z; az++)
+putchar(az);
+for (az = A; az <= Z; az++)
+putchar(az);
+putchar(n);
+return (0);
+}
