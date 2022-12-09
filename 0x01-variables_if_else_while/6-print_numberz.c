67afb86aeb71779a75ef3a6cd14f67b98b6b22d6
Saviourise <saviourise1@gmail.com>
2022-12-09 08:14:58 -0800

    If 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main- prints 1-9.
+ * Return: 0 if successful
+ */
+int main(void)
+{
+int n;
+for (n = 48; n < 58; n++)
+{
+putchar(n);
+}
+putchar("\n");
+return (0);
+}
