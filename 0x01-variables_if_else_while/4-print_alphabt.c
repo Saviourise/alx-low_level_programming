188683d1d42eee2b21eb4fca30e2191b0693206b
Saviourise <saviourise1@gmail.com>
2022-12-09 08:04:38 -0800

    If 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main- all lower case alphabets without e qnd q.
+ * Return: 0 if successful.
+ */
+
+int main(void)
+{
+char az = a;
+while (az <= z)
+{
+if (az != e && az != q)
+{
+putchar(az);
+}
+az++;
+}
+putchar(n);
+return (0);
+}
