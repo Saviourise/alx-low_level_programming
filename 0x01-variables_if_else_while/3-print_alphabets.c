bf478d66b9dd052c3cdf5f71a5242b99abdc5b8e
Saviourise <65181992+Saviourise@users.noreply.github.com>
2022-12-09 17:00:00 +0100

    If 3

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -9,10 +9,10 @@
 int main(void)
 {
 char az;
-for (az = a; az <= z; az++)
+for (az = 'a'; az <= 'z'; az++)
 putchar(az);
-for (az = A; az <= Z; az++)
+for (az = 'A'; az <= 'Z'; az++)
 putchar(az);
-putchar(n);
+putchar('\n');
 return (0);
 }
