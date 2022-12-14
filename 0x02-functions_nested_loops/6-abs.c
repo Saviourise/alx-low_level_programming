d2a1ca0a16ce8de57b972ba20f8573aacb437bbb
Saviourise <65181992+Saviourise@users.noreply.github.com>
2022-12-14 03:31:50 +0100

    Create 6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+#include <math.h>
+/**
+ * _abs- prints the absolute of the argument
+ * @int: parameter
+ *
+ * Return: 0 if successful
+ */
+int _abs(int);
+
+int _abs(int x)
+{
+	int abs(int x);
+	if (x < 0)
+	{
+		x *= -1;
+	}
+	return (x);
+}
