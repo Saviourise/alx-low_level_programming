ef29ff7c56af04451bcff38eefdaeb6e14180f92
Saviourise <65181992+Saviourise@users.noreply.github.com>
2022-12-14 03:40:38 +0100

    Create 10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <math.h>
+#include "main.h"
+/**
+ * add- adds two arguments together
+ * @int: parameter 1
+ * @int: parameter 2
+ *
+ * Return: z
+ */
+
+int add(int, int);
+
+int add(int x, int y)
+{
+	int z;
+
+	z = x + y;
+	return (z);
+}
