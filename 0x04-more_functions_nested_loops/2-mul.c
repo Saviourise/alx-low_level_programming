8f0f124fee13fa7bc3b266a2b920229da0a856ba
Saviourise <65181992+Saviourise@users.noreply.github.com>
2022-12-16 00:22:49 +0100

    Create 2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include "main.h"
+/**
+ * mul- multiplies two arguments together
+ * @a: parameter 1.
+ * @b: parameter 2.
+ *
+ * Return: void;
+ */
+
+int mul(int a, int b)
+{
+	int c;
+
+	c = a * b;
+	return (c);
+}
