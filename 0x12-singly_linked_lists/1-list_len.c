09ac8aecec6a95ee68af4346cb0cea95384132f0
Saviourise <65181992+Saviourise@users.noreply.github.com>
2023-01-26 23:36:39 +0100

    Update 1-list_len.c

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,19 +1,22 @@
 #include "lists.h"
+
 /**
- * list_len - returns then number of elements in a list.
- * @h: singly linked list.
- * Return: number of elements in the list.
+ * list_len - returns the number of elements
+ * @h: parameter
+ *
+ * Return: number of elements
  */
 
 size_t list_len(const list_t *h)
 {
-	size_t nelem;
+	size_t num;
 
-	nelem = 0;
+	num = 0;
+
 	while (h != NULL)
 	{
 		h = h->next;
-		nelem++;
+		num++;
 	}
-	return (nelem);
+	return (num);
 }
